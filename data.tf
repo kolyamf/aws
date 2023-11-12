4668ee929df38ee585d2a6f3a12208913e667e80
kolyamf <kolyamf@gmail.com>
2023-11-12 13:38:21 +0200

    aws-2: uncomment data ami

--- data.tf
+++ data.tf
@@ -2,15 +2,15 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
-#   filter {
-#     name   = "name"
-#     values = ["golden_image-*"]
-#   }
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-#   owners = ["self"]
-# }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["golden_image-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["self"]
+}
